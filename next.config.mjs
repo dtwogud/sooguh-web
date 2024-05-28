78adc1d47cb2903f12df4ce644fd63f1eed90dd4
dtwogud@nate.com <dtwogud@nate.com>
2024-05-28 13:38:13 +0900

    chore: nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,39 @@
 /** @type {import('next').NextConfig} */
+import withBundleAnalyzer from '@next/bundle-analyzer';
 
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-    enabled: process.env.ANALYZE === "true",
-});
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//     enabled: process.env.ANALYZE === "true",
+// });
 
-module.exports = (phase, { defaultConfig }) => {
-    const config = {
-        reactStrictMode: true,
-        swcMinify: true,
-        poweredByHeader: false,
-        compiler: {
-            removeConsole: phase !== PHASE_DEVELOPMENT_SERVER,
-        },
-        webpack: (config) => {
-            config.module.rules.push({
-                test: /\.svg$/i,
-                issuer: /\.[jt]sx?$/,
-                use: ["@svgr/webpack"],
-            });
-            return config;
-        },
-        images: {
-            unoptimized: true,
-        },
-        async rewrites() {
-            return [
-                {
-                    source: `/api/:path*`,
-                    destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
-                },
-            ];
-        },
-    };
-    return withBundleAnalyzer(config);
+const bundleAnalyzer = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    poweredByHeader: false,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === "production",
+    },
+    webpack: (config) => {
+        config.module.rules.push({
+            test: /\.svg$/i,
+            issuer: /\.[jt]sx?$/,
+            use: ["@svgr/webpack"],
+        });
+        return config;
+    },
+    //TODO images: {
+    //     unoptimized: true,
+    // },
+    async rewrites() {
+        return [
+            {
+                source: `/api/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+            },
+        ];
+    },
 };
+export default bundleAnalyzer(nextConfig);
