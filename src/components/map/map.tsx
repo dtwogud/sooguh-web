daa6f29d731df9b8bb4938aa40db6778b20d7f1a
dtwogud@nate.com <dtwogud@nate.com>
2024-06-04 13:24:14 +0900

    feat: useCoords

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,42 +1,80 @@
 "use client";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import {
   Map as KakaoMap,
   MapMarker,
   useKakaoLoader,
 } from "react-kakao-maps-sdk";
+import useCoords, { ICoordsState } from "@/src/hooks/useCoords";
+
+export interface DetailData {
+  id: number;
+  dong: string;
+  address: string;
+  lat: number;
+  lon: number;
+}
+
+const dummyData = {
+  info: [
+    {
+      id: 1,
+      dong: "송파구",
+      address: "서울특별시 송파구 가락로7길 21",
+      lat: 37.5018105,
+      lon: 127.1027394,
+    },
+    {
+      id: 2,
+      dong: "송파구",
+      address: "서울특별시 송파구 가락로11길 26",
+      lat: 37.5027584,
+      lon: 127.1048724,
+    },
+  ],
+};
 
 const Map = () => {
   const [loading, error] = useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_KAKAO_APP_KEY!,
   });
+  const curCoords = useCoords("waring");
+  const [coords, setCoords] = useState<ICoordsState>(curCoords);
+
+  useEffect(() => {
+    if (curCoords) setCoords(curCoords);
+  }, [curCoords]);
+
+  const handleOnBoundsChange = useCallback((data: kakao.maps.Map) => {
+    console.log("data", data);
+  }, []);
+
+  const handleMarkerClick = (marker: kakao.maps.Marker, data: DetailData) => {
+    console.log("marker", marker);
+    console.log("data", data);
+  };
 
   return (
     <>
-      <div className="w-[100%] h-[100vh]">
-        test map
+      <div className="w-[100%] h-[90vh]">
         <KakaoMap
-          center={{ lat: 37.45857097793072, lng: 126.89738261529611 }}
+          center={{
+            lat: coords.latitude ?? 37.5664056,
+            lng: coords.longitude ?? 126.9778222,
+          }}
           className={"w-[100%] h-[100%]"}
+          level={3}
+          onBoundsChanged={(data) => handleOnBoundsChange(data)}
         >
-          <MapMarker
-            position={{ lat: 37.45857097793072, lng: 126.89738261529611 }}
-          >
-            <div className={"test"} style={{ backgroundColor: "pink" }}>
-              혠 하우스~!
-            </div>
-          </MapMarker>
-          {/* TODO marker cluster 처리  <MarkerClusterer
-        averageCenter={true}
-        minLevel={10}
-      >
-        {clusterPositionsData.positions.map((pos) => (
-          <MapMarker
-            key={`${pos.lat}-${pos.lng}`}
-            position={pos}
-          />
-        ))}
-      </MarkerClusterer>*/}
+          {dummyData.info.map((data) => {
+            return (
+              <MapMarker
+                key={`${data.lat}-${data.lon}`}
+                position={{ lat: data.lat, lng: data.lon }}
+                onClick={(marker) => handleMarkerClick(marker, data)}
+              />
+            );
+          })}
         </KakaoMap>
       </div>
     </>
---
+++ src/hooks/useCoords.tsx
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useState } from "react";
+
+export interface ICoordsState {
+  latitude: number | null;
+  longitude: number | null;
+}
+
+const useCoords = (warningText: string) => {
+  const [coords, setCoords] = useState<ICoordsState>({
+    latitude: null,
+    longitude: null,
+  });
+
+  // 위치 허용 시 실행
+  const onSuccess = useCallback(
+    ({ coords: { latitude, longitude } }: GeolocationPosition) =>
+      setCoords({ latitude, longitude }),
+    [],
+  );
+
+  // 위치 미허용 시 실행
+  const onFailure = useCallback(
+    (error: GeolocationPositionError) => alert(`${warningText} >> ${error}`),
+    [warningText],
+  );
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(onSuccess, onFailure);
+  }, [onSuccess, onFailure]);
+
+  return coords;
+};
+
+export default useCoords;
