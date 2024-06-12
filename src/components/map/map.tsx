c5777b58be838777bc7b19b80afaa7f5f255e54e
MJ <81230679+dtwogud@users.noreply.github.com>
2024-06-12 14:47:04 +0900

    fix: 현재 위치 표시

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -9,6 +9,7 @@ import useCoords, { ICoordsState } from "@/src/hooks/useCoords";
 import SearchAddressBar from "@/src/components/map/search-address-bar";
 import useModal from "@/src/hooks/useModal";
 import PinModal from "@/src/components/main/pin-modal";
+import useResize from "@/src/hooks/useResize";
 
 export interface DetailData {
   id: number;
@@ -44,13 +45,24 @@ const Map = () => {
   });
   const curCoords = useCoords("waring");
   const [coords, setCoords] = useState<ICoordsState>(curCoords);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
   const [detailData, setDetailData] = useState<DetailData | undefined>(
     undefined,
   );
   const { openModal, onModalOpen, onModalClose } = useModal();
 
+  useResize(() => {
+    if (window.innerWidth > 640) setIsMobile(false);
+    else if (window.innerWidth < 640) setIsMobile(true);
+  });
+
   useEffect(() => {
-    if (curCoords) setCoords(curCoords);
+    if (!curCoords) setIsLoading(true);
+    if (curCoords) {
+      setCoords(curCoords);
+      setIsLoading(false);
+    }
   }, [curCoords]);
 
   const handleOnBoundsChange = useCallback((data: kakao.maps.Map) => {
@@ -64,33 +76,48 @@ const Map = () => {
 
   return (
     <>
-      <div className="w-[100%] h-[70vh]">
-        <KakaoMap
-          center={{
-            lat: coords.latitude ?? 37.5664056,
-            lng: coords.longitude ?? 126.9778222,
-          }}
-          className={"w-[100%] h-[100%]"}
-          level={3}
-          //TODO onBoundsChanged={(data) => handleOnBoundsChange(data)}
-        >
-          <SearchAddressBar />
-          {dummyData.info.map((data) => {
-            return (
-              <MapMarker
-                key={`${data.lat}-${data.lon}`}
-                position={{ lat: data.lat, lng: data.lon }}
-                onClick={(marker) => handleMarkerClick(marker, data)}
-              />
-            );
-          })}
-          <PinModal
-            data={detailData}
-            openModal={openModal}
-            onModalClose={onModalClose}
-          />
-        </KakaoMap>
-      </div>
+      {isLoading ? (
+        <>Loading...</>
+      ) : (
+        <div className="w-[100%] h-[70vh]">
+          <KakaoMap
+            center={{
+              lat: coords.latitude ?? 37.5664056,
+              lng: coords.longitude ?? 126.9778222,
+            }}
+            className={"w-[100%] h-[100%]"}
+            level={3}
+            //TODO onBoundsChanged={(data) => handleOnBoundsChange(data)}
+          >
+            <SearchAddressBar />
+            <MapMarker
+              key={`${coords.latitude ?? 37.5664056}-${coords.longitude ?? 126.9778222}`}
+              position={{
+                lat: coords.latitude ?? 37.5664056,
+                lng: coords.longitude ?? 126.9778222,
+              }}
+              image={{
+                src: "/assets/icons/location.png",
+                size: { width: isMobile ? 32 : 64, height: isMobile ? 32 : 64 },
+              }}
+            />
+            {dummyData.info.map((data) => {
+              return (
+                <MapMarker
+                  key={`${data.lat}-${data.lon}`}
+                  position={{ lat: data.lat, lng: data.lon }}
+                  onClick={(marker) => handleMarkerClick(marker, data)}
+                />
+              );
+            })}
+            <PinModal
+              data={detailData}
+              openModal={openModal}
+              onModalClose={onModalClose}
+            />
+          </KakaoMap>
+        </div>
+      )}
     </>
   );
 };
