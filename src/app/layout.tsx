1cd7940182d388a4853929fba3a52878bac68e5c
dtwogud@nate.com <dtwogud@nate.com>
2024-07-03 16:02:18 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,24 +2,35 @@ import type { Metadata } from "next";
 import "@/src/styles/globals.css";
 import "@/src/styles/reset.css";
 import { CoordsProvider } from "@/src/context/coords.context";
-import { AppLayoutProps } from "@/index";
+import React from "react";
+import { BASIC_COORDS } from "@/src/constants/basic-coords";
 
 export const metadata: Metadata = {
   title: "SOOGUH",
   description: "우리 주변의 헌웃 수거함을 찾아보아요.",
 };
-
-export default function RootLayout(props: AppLayoutProps) {
+export default function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params?: string;
+}) {
   return (
     <html lang="ko">
       <body>
-        <CoordsProvider coords={props.coords}>
+        <CoordsProvider
+          coords={{
+            latitude: BASIC_COORDS.latitude,
+            longitude: BASIC_COORDS.longitude,
+          }}
+        >
           <main
             className={
               "lg:p-[16px] sm:p-[8px] min-w-full min-h-screen grid relative overflow-scroll"
             }
           >
-            {props.children}
+            {children}
           </main>
         </CoordsProvider>
       </body>
